97f68a93ebb1563b77df6d20d577916f5e1b6bc6
tregubovao <114154901+tregubovao@users.noreply.github.com>
2023-09-13 12:08:56 +0300

    Update task2.py

--- HW1/task2.py
+++ HW1/task2.py
@@ -5,12 +5,16 @@ import string
 def find_text(command: str, text: str) -> bool:
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
     out = result.stdout
-    out = (out.translate(str.maketrans('', '', string.punctuation)))
-    my_list = out.split('\n')
+    for el in string.punctuation:
+        out = out.replace(el, ' ')
+    my_list = out.split()
     if text in my_list and result.returncode == 0:
         return True
     return False
 
 
 if __name__ == '__main__':
-    print(find_text('cat /etc/os-release', 'NAMEUbuntu'))
+    print(find_text('cat /etc/os-release', 'Ubuntu'))
+    print(find_text('cat /etc/os-release', 'VERSION'))
+    print(find_text('cat /etc/os-release', 'legal'))
+
